e1722441f78db4ab7fc95a442890830abac6f3cd
J0Min <rudals489@gmail.com>
2026-06-01 16:35:24 +0900

    refactor: 3인칭 플레이어 마우스회전+전후방 입력 간섭에 따른 축 변경 방어 코드 작성

--- Source/TPSProject/Private/TPSPlayer.cpp
+++ Source/TPSProject/Private/TPSPlayer.cpp
@@ -50,13 +50,30 @@ void ATPSPlayer::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	
+	
+	/* GetContolRotation() 함수는 좌우(Yaw) 말고도 상하(Pitch)까지 포함된 카메라 전체 회전
+	Pitch 움직임에 X축을 기울이는 성질이 있어서 카메라가 아래를 향하는 상태에서 w입력시
+	"앞으로 가는 입력" + "아래로 박는 입력"으로 섞임 -> 수평속도가 cos(Pitch)로 줄어듬 */
+	
+	
+	//컨트롤러의 현재 회전 값들 (Yaw, Pitch, Roll)값을 가져옴
+	FRotator controlRot = GetControlRotation();
+	//Pitch자체를 0으로 설정 -> 기우는 현상 차단 -> 일정속도 이동
+	controlRot.Pitch = 0.0f;
+	controlRot.Roll = 0.0f;
+	//원래도 0이긴함 -> 카메라 움직임에 따라에도 움직일 가능성 존재하여 사전 차단
+	
+	//Yaw 좌우 측값만 남은 벡터에 입력 -> 카메라의 회전과 관계엾이 수평면 위에서만 이동
+	direction = FRotationMatrix(controlRot).TransformVector(direction);
+	
 	//플레이어 이동 처리
 	//P(결과위치) = P0(초기위치) + v(속도) * t(시간)
-	direction = FTransform(GetControlRotation()).TransformFVector4(direction);
 	// FVector P0 = GetActorLocation();
 	// FVector vt = direction * walkSpeed * DeltaTime;
 	// FVector P = P0 + vt;
 	// SetActorLocation(P);
+	
+	//direction = FTransform(GetControlRotation()).TransformFVector4(direction);
 	AddMovementInput(direction);
 	direction = FVector::ZeroVector;
 }
