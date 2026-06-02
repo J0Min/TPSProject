7f9dc3e71080f19c5a97f5f1e9271b9ccc355bd3
J0Min <rudals489@gmail.com>
2026-06-02 14:19:39 +0900

    feat: Bullet 자동 소멸 구현(InitialLifeSpan / FTimerManager를 통한 방식)

--- Source/TPSProject/Private/TPSPlayer.cpp
+++ Source/TPSProject/Private/TPSPlayer.cpp
@@ -1,8 +1,126 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
+// // Fill out your copyright notice in the Description page of Project Settings.
+//
+//
+// #include "TPSProject/Public/TPSPlayer.h"
+//
+// #include "EnhancedInputComponent.h"
+// #include "EnhancedInputSubsystems.h"
+// #include "Camera/CameraComponent.h"
+// #include "GameFramework/SpringArmComponent.h"
+//
+//
+// // Sets default values
+// ATPSPlayer::ATPSPlayer()
+// {
+// 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+// 	PrimaryActorTick.bCanEverTick = true;
+// 	
+// 	//TPS 카메라를 SpringArm에 부착
+// 	springArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+// 	springArmComp->SetupAttachment(RootComponent);//루트에 자식
+// 	springArmComp->SetRelativeLocation(FVector(0.0f, 70.0f, 90.0f));//시작 위치 변경
+// 	springArmComp->TargetArmLength = 400.0f; //암 길이
+// 	
+// 	//카메라 컴포넌트
+// 	cameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+// 	cameraComp->SetupAttachment(springArmComp);
+// 	
+// }
+//
+// // Called when the game starts or when spawned
+// void ATPSPlayer::BeginPlay()
+// {
+// 	Super::BeginPlay();
+// 	
+// 	//Enhanced Input 시스템이 IMC_TPS 사용하도록 설정
+// 	auto pc = Cast<APlayerController>(Controller);
+// 	if (pc)
+// 	{
+// 		auto subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(pc->GetLocalPlayer());
+// 		if (subsystem)
+// 		{
+// 			subsystem->AddMappingContext(imc_TPS, 0);
+// 		}
+// 	}
+// 	
+// }
+//
+// // Called every frame
+// void ATPSPlayer::Tick(float DeltaTime)
+// {
+// 	Super::Tick(DeltaTime);
+// 	
+// 	
+// 	/* GetContolRotation() 함수는 좌우(Yaw) 말고도 상하(Pitch)까지 포함된 카메라 전체 회전
+// 	Pitch 움직임에 X축을 기울이는 성질이 있어서 카메라가 아래를 향하는 상태에서 w입력시
+// 	"앞으로 가는 입력" + "아래로 박는 입력"으로 섞임 -> 수평속도가 cos(Pitch)로 줄어듬 */
+// 	
+// 	
+// 	//컨트롤러의 현재 회전 값들 (Yaw, Pitch, Roll)값을 가져옴
+// 	FRotator controlRot = GetControlRotation();
+// 	//Pitch자체를 0으로 설정 -> 기우는 현상 차단 -> 일정속도 이동
+// 	controlRot.Pitch = 0.0f;
+// 	controlRot.Roll = 0.0f;
+// 	//원래도 0이긴함 -> 카메라 움직임에 따라에도 움직일 가능성 존재하여 사전 차단
+// 	
+// 	//Yaw 좌우 측값만 남은 벡터에 입력 -> 카메라의 회전과 관계엾이 수평면 위에서만 이동
+// 	direction = FRotationMatrix(controlRot).TransformVector(direction);
+// 	
+// 	//플레이어 이동 처리
+// 	//P(결과위치) = P0(초기위치) + v(속도) * t(시간)
+// 	// FVector P0 = GetActorLocation();
+// 	// FVector vt = direction * walkSpeed * DeltaTime;
+// 	// FVector P = P0 + vt;
+// 	// SetActorLocation(P);
+// 	
+// 	//direction = FTransform(GetControlRotation()).TransformFVector4(direction);
+// 	AddMovementInput(direction);
+// 	direction = FVector::ZeroVector;
+// }
+//
+// // Called to bind functionality to input
+// void ATPSPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+// {
+// 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+// 	
+// 	auto PlayerInput = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+// 	if (PlayerInput)
+// 	{
+// 		PlayerInput -> BindAction(ia_Move, ETriggerEvent::Triggered, this, &ATPSPlayer::Move);
+// 		PlayerInput -> BindAction(ia_LookUP, ETriggerEvent::Triggered, this, &ATPSPlayer::LookUP);
+// 		PlayerInput -> BindAction(ia_Turn, ETriggerEvent::Triggered, this, &ATPSPlayer::Turn);
+// 		PlayerInput -> BindAction(ia_Jump, ETriggerEvent::Started, this, &ATPSPlayer::InputJump);
+// 	}
+// }
+//
+// void ATPSPlayer::Move(const struct FInputActionValue& inputValue)
+// {
+// 	FVector2D value = inputValue.Get<FVector2D>();
+// 	direction.X = value.X;//전후
+// 	direction.Y = value.Y;//좌우
+// }
+//
+// void ATPSPlayer::LookUP(const struct FInputActionValue& inputValue)
+// {
+// 	float value = inputValue.Get<float>();
+// 	AddControllerPitchInput(value);// Y축 회전
+// }
+//
+// void ATPSPlayer::Turn(const struct FInputActionValue& inputValue)
+// {
+// 	float value = inputValue.Get<float>();
+// 	AddControllerYawInput(value);// Z축 회전
+// }
+//
+// void ATPSPlayer::InputJump(const struct FInputActionValue& inputValue)
+// {
+// 	//ACharater 내부의 기본 제공 점프 함수
+// 	Jump();
+// }
+//
 #include "TPSProject/Public/TPSPlayer.h"
 
+#include "Bullet.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include "Camera/CameraComponent.h"
@@ -15,16 +133,32 @@ ATPSPlayer::ATPSPlayer()
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	
-	//TPS 카메라를 SpringArm에 부착
+	// TPS 카메라를 SpringArm 컴포넌트에 부착
 	springArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
-	springArmComp->SetupAttachment(RootComponent);//루트에 자식
-	springArmComp->SetRelativeLocation(FVector(0.0f, 70.0f, 90.0f));//시작 위치 변경
-	springArmComp->TargetArmLength = 400.0f; //암 길이
+	springArmComp->SetupAttachment(RootComponent); // 계층 구조상 캡슐컴포넌트가 ROOT이며 스프링암을 자식으로 배치
+	springArmComp->SetRelativeLocation(FVector(.0f, 70.0f, 90.0f)); // 암 컴포넌트의 시작점
+	springArmComp->TargetArmLength = 400.f;
 	
-	//카메라 컴포넌트
 	cameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
 	cameraComp->SetupAttachment(springArmComp);
 	
+	// C++에서 BP에서의 옵션들 직접 수정하는 경우 아래처럼 해당 옵션 변수들을 직접 코드로 제어 가능
+	// springArmComp->bUsePawnControlRotation = true;
+	// cameraComp->bUsePawnControlRotation = false;
+	// bUseControllerRotationYaw = true;
+	
+	// 총 스켈레탈메시 컴포넌트 등록
+	gunMeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("GunMeshComponent"));
+	// 캐릭터 메시 컴포넌트(GetMesh()) 부모에 부착
+	gunMeshComp->SetupAttachment(GetMesh());
+	// 스켈레탈 메시 데이터 동적로드
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> TempGunMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Weapons/GrenadeLauncher/Meshes/SKM_GrenadeLauncher.SKM_GrenadeLauncher'"));
+	if (TempGunMesh.Succeeded())
+	{
+		// 해당 경로의 스켈레탈메시를 찾았다면, 메시 할당 + 임시 위치 보정
+		gunMeshComp->SetSkeletalMesh(TempGunMesh.Object);
+		gunMeshComp->SetRelativeLocation(FVector(-14.0f, 52.0f, 120.0f));
+	}
 }
 
 // Called when the game starts or when spawned
@@ -32,7 +166,7 @@ void ATPSPlayer::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	//Enhanced Input 시스템이 IMC_TPS 사용하도록 설정
+	// Enhanced Input 시스템이 IMC_TPS 사용하도록 설정
 	auto pc = Cast<APlayerController>(Controller);
 	if (pc)
 	{
@@ -42,7 +176,6 @@ void ATPSPlayer::BeginPlay()
 			subsystem->AddMappingContext(imc_TPS, 0);
 		}
 	}
-	
 }
 
 // Called every frame
@@ -50,30 +183,22 @@ void ATPSPlayer::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	
+	// 캐릭터 이동처리에서 버그 예시)
+	// GetControlRation() 함수는 좌우(YAW) 말고도 상하(PITCH)까지 포함된 카메라 전체 회전
+	// PITCH 움직임에 X축을 기울이는 성질이 있어서 카메라가 아래를 향하는 상태에서 W를 누르면
+	// "앞으로 가는 입력" + "아래로 박는 입력"으로 섞임 -> 수평 속도가 cos(Pitch)로 줄어듬
 	
-	/* GetContolRotation() 함수는 좌우(Yaw) 말고도 상하(Pitch)까지 포함된 카메라 전체 회전
-	Pitch 움직임에 X축을 기울이는 성질이 있어서 카메라가 아래를 향하는 상태에서 w입력시
-	"앞으로 가는 입력" + "아래로 박는 입력"으로 섞임 -> 수평속도가 cos(Pitch)로 줄어듬 */
-	
-	
-	//컨트롤러의 현재 회전 값들 (Yaw, Pitch, Roll)값을 가져옴
+	// 컨트롤러의 현재 회전 값들(YAW, PITCH, ROLL) 값을 가져옴
 	FRotator controlRot = GetControlRotation();
-	//Pitch자체를 0으로 설정 -> 기우는 현상 차단 -> 일정속도 이동
-	controlRot.Pitch = 0.0f;
-	controlRot.Roll = 0.0f;
-	//원래도 0이긴함 -> 카메라 움직임에 따라에도 움직일 가능성 존재하여 사전 차단
+	// PITCH 자체를 0으로 - 기우는 현상 차단
+	controlRot.Pitch = 0.f;
+	// ROLL 도 0으로 고정 - 기우는 현상 차단
+	controlRot.Roll = 0.f;
 	
-	//Yaw 좌우 측값만 남은 벡터에 입력 -> 카메라의 회전과 관계엾이 수평면 위에서만 이동
-	direction = FRotationMatrix(controlRot).TransformVector(direction);
+	// YAW좌우 측값만 남은 벡터를 넣어둠 -> 카메라가 어디를 보던지, 이동은 수평면 위에서만 이루어지도록 함
+	direction = FRotationMatrix(controlRot).TransformVector(direction); // 자기를 기준으로 벡터 변환
 	
-	//플레이어 이동 처리
-	//P(결과위치) = P0(초기위치) + v(속도) * t(시간)
-	// FVector P0 = GetActorLocation();
-	// FVector vt = direction * walkSpeed * DeltaTime;
-	// FVector P = P0 + vt;
-	// SetActorLocation(P);
-	
-	//direction = FTransform(GetControlRotation()).TransformFVector4(direction);
+	// 언리얼엔진에서 제공하는 위 등속 운동을 구현한 함수 AddMovementInput()
 	AddMovementInput(direction);
 	direction = FVector::ZeroVector;
 }
@@ -86,35 +211,46 @@ void ATPSPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 	auto PlayerInput = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
 	if (PlayerInput)
 	{
-		PlayerInput -> BindAction(ia_Move, ETriggerEvent::Triggered, this, &ATPSPlayer::Move);
-		PlayerInput -> BindAction(ia_LookUP, ETriggerEvent::Triggered, this, &ATPSPlayer::LookUP);
-		PlayerInput -> BindAction(ia_Turn, ETriggerEvent::Triggered, this, &ATPSPlayer::Turn);
-		PlayerInput -> BindAction(ia_Jump, ETriggerEvent::Started, this, &ATPSPlayer::InputJump);
+		PlayerInput->BindAction(ia_LookUp, ETriggerEvent::Triggered, this, &ATPSPlayer::LookUp);
+		PlayerInput->BindAction(ia_Turn, ETriggerEvent::Triggered, this, &ATPSPlayer::Turn);
+		PlayerInput->BindAction(ia_Move, ETriggerEvent::Triggered, this, &ATPSPlayer::Move);
+		PlayerInput->BindAction(ia_Jump, ETriggerEvent::Started, this, &ATPSPlayer::InputJump);
+		PlayerInput->BindAction(ia_Fire, ETriggerEvent::Started, this, &ATPSPlayer::InputFire);
 	}
 }
 
-void ATPSPlayer::Move(const struct FInputActionValue& inputValue)
-{
-	FVector2D value = inputValue.Get<FVector2D>();
-	direction.X = value.X;//전후
-	direction.Y = value.Y;//좌우
-}
-
-void ATPSPlayer::LookUP(const struct FInputActionValue& inputValue)
+// 상하 회전 입력에 따른 콜백 함수 구현
+void ATPSPlayer::LookUp(const FInputActionValue& inputValue)
 {
 	float value = inputValue.Get<float>();
-	AddControllerPitchInput(value);// Y축 회전
+	AddControllerPitchInput(value); // PITCH(Y축) 회전
 }
 
-void ATPSPlayer::Turn(const struct FInputActionValue& inputValue)
+// 좌우 회전 입력에 따른 콜백 함수 구현
+void ATPSPlayer::Turn(const FInputActionValue& inputValue)
 {
 	float value = inputValue.Get<float>();
-	AddControllerYawInput(value);// Z축 회전
+	AddControllerYawInput(value); // YAW(Z축) 회전
 }
 
-void ATPSPlayer::InputJump(const struct FInputActionValue& inputValue)
+// 전후좌우 이동 입력에 따른 콜백 함수 구현
+void ATPSPlayer::Move(const FInputActionValue& inputValue)
 {
-	//ACharater 내부의 기본 제공 점프 함수
-	Jump();
+	FVector2D value = inputValue.Get<FVector2D>(); // 전달받는 2D 값
+	direction.X = value.X; // 전후
+	direction.Y = value.Y; // 좌우
 }
 
+// 점프 입력에 따른 콜백 함수 구현
+void ATPSPlayer::InputJump(const FInputActionValue& inputValue)
+{
+	Jump(); // ACharacter 클래스가 제공하는 기본 점프 함수 호출
+}
+
+void ATPSPlayer::InputFire(const FInputActionValue& inputValue)
+{
+	// 총 스켈레탈메시에 FirePosition 이란 이름의 소켓의 월드 트랜스폼(위치/회전)을 가져옴
+	FTransform firePosition = gunMeshComp->GetSocketTransform(TEXT("FirePosition"));
+	// 위 위치/회전으로 BulletFactory가 BP_Bullet 인스턴스를 월드에 스폰
+	GetWorld()->SpawnActor<ABullet>(bulletFactory, firePosition);
+}
--- Source/TPSProject/Public/TPSPlayer.h
+++ Source/TPSProject/Public/TPSPlayer.h
@@ -1,4 +1,67 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// // Fill out your copyright notice in the Description page of Project Settings.
+//
+// #pragma once
+//
+// #include "CoreMinimal.h"
+// #include "GameFramework/Character.h"
+// #include "TPSPlayer.generated.h"
+//
+// UCLASS()
+// class TPSPROJECT_API ATPSPlayer : public ACharacter
+// {
+// 	GENERATED_BODY()
+//
+// public:
+// 	// Sets default values for this character's properties
+// 	ATPSPlayer();
+//
+// protected:
+// 	// Called when the game starts or when spawned
+// 	virtual void BeginPlay() override;
+//
+// public:
+// 	// Called every frame
+// 	virtual void Tick(float DeltaTime) override;
+//
+// 	// Called to bind functionality to input
+// 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+// 	
+// 	//스프링 암 컴포넌트
+// 	UPROPERTY(VisibleAnywhere, Category = Camera)
+// 	class USpringArmComponent* springArmComp;
+// 	
+// 	//카메라 컴포넌트
+// 	UPROPERTY(VisibleAnywhere, Category = Camera)
+// 	class UCameraComponent* cameraComp;
+//
+// 	//입력부
+// 	UPROPERTY(EditDefaultsOnly, Category = Input)
+// 	class UInputMappingContext* imc_TPS;
+// 	
+// 	UPROPERTY(EditDefaultsOnly, Category = Input)
+// 	class UInputAction* ia_LookUP;
+// 	
+// 	UPROPERTY(EditDefaultsOnly, Category = Input)
+// 	class UInputAction* ia_Turn;
+// 	
+// 	UPROPERTY(EditDefaultsOnly, Category = Input)
+// 	class UInputAction* ia_Move;
+// 	
+// 	UPROPERTY(EditDefaultsOnly, Category = Input)
+// 	class UInputAction* ia_Jump;
+// 	
+// 	//이동 속도
+// 	UPROPERTY(EditDefaultsOnly, Category = PlayerSetting)
+// 	float walkSpeed = 600.f;
+// 	
+// 	//이동 방향
+// 	FVector direction;
+// 	
+// 	void Move(const struct FInputActionValue& inputValue);
+// 	void LookUP(const struct FInputActionValue& inputValue);
+// 	void Turn(const struct FInputActionValue& inputValue);
+// 	void InputJump(const struct FInputActionValue& inputValue);
+// };
 
 #pragma once
 
@@ -26,39 +89,64 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	
-	//스프링 암 컴포넌트
+	// 스프링 암 컴포넌트 선언
 	UPROPERTY(VisibleAnywhere, Category = Camera)
 	class USpringArmComponent* springArmComp;
 	
-	//카메라 컴포넌트
+	// 카메라 컴포넌트 선언
 	UPROPERTY(VisibleAnywhere, Category = Camera)
 	class UCameraComponent* cameraComp;
-
-	//입력부
+	
+	// 총 스켈레탈 메시 컴포넌트 선언
+	UPROPERTY(VisibleAnywhere, Category = GunMesh)
+	class USkeletalMeshComponent* gunMeshComp;
+	
+	// IMC 선택 필드 선언
 	UPROPERTY(EditDefaultsOnly, Category = Input)
 	class UInputMappingContext* imc_TPS;
 	
+	// 상하좌우 마우스 IA 선택 필드 선언
 	UPROPERTY(EditDefaultsOnly, Category = Input)
-	class UInputAction* ia_LookUP;
+	class UInputAction* ia_LookUp;
 	
 	UPROPERTY(EditDefaultsOnly, Category = Input)
 	class UInputAction* ia_Turn;
 	
+	// 전후좌우 키보드 이동 IA 필드 선언
 	UPROPERTY(EditDefaultsOnly, Category = Input)
 	class UInputAction* ia_Move;
-	
+
+	// 키보드 점프 IA 필드 선언
 	UPROPERTY(EditDefaultsOnly, Category = Input)
 	class UInputAction* ia_Jump;
 	
-	//이동 속도
+	// 마우스 발사 IA 필드 선언
+	UPROPERTY(EditDefaultsOnly, Category = Input)
+	class UInputAction* ia_Fire;
+	
+	// 이동 속도
 	UPROPERTY(EditDefaultsOnly, Category = PlayerSetting)
 	float walkSpeed = 600.f;
 	
-	//이동 방향
+	// 총알 스폰 팩토리
+	UPROPERTY(EditDefaultsOnly, Category = BulletFactory)
+	TSubclassOf<class ABullet> bulletFactory;
+	
+	// 이동 방향
 	FVector direction;
 	
-	void Move(const struct FInputActionValue& inputValue);
-	void LookUP(const struct FInputActionValue& inputValue);
+	// 상하 회전 입력 함수 선언
+	void LookUp(const struct FInputActionValue& inputValue);
+	
+	// 좌우 회전 입력 함수 선언
 	void Turn(const struct FInputActionValue& inputValue);
+	
+	// 전후좌우 이동 입력 함수 선언
+	void Move(const struct FInputActionValue& inputValue);
+	
+	// 점프 입력 함수 선언
 	void InputJump(const struct FInputActionValue& inputValue);
+	
+	// 총알 발사 입력 함수 선언
+	void InputFire(const struct FInputActionValue& inputValue);
 };
